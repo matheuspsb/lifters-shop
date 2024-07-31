dc287b4556bf6ca05842650544a172a707da8133
Matheus <matheus.souza@teamsoft.com.br>
2024-07-31 01:19:19 -0300

    refactor: navbar function navigation

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Bag, MagnifyingGlass } from "@phosphor-icons/react";
 import "./Navbar.css";
 
@@ -7,10 +8,12 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = ({ totalItems }) => {
+  const navigate = useNavigate();
+
   return (
     <nav className="navbar">
       <div className="navbar-left">
-        <span className="navbar-brand">Lifters Shop</span>
+        <span onClick={() => navigate("/")} className="navbar-brand">Lifters Shop</span>
         <ul className="navbar-nav">
           <li className="nav-item">Shop</li>
           <li className="nav-item">Stories</li>
