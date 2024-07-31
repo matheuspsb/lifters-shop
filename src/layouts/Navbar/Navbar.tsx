dd22782062683c46b9439bb64e83308fc2d88111
Matheus <matheus.souza@teamsoft.com.br>
2024-07-31 03:07:35 -0300

    refactor: navbar total items props

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,19 +1,18 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useCart } from "../../contexts/CartContext";
 import { Bag, MagnifyingGlass } from "@phosphor-icons/react";
 import "./Navbar.css";
 
-interface NavbarProps {
-  totalItems: number;
-}
-
-const Navbar: React.FC<NavbarProps> = ({ totalItems }) => {
+const Navbar = () => {
   const navigate = useNavigate();
+  const { state } = useCart();
 
   return (
     <nav className="navbar">
       <div className="navbar-left">
-        <span onClick={() => navigate("/")} className="navbar-brand">Lifters Shop</span>
+        <span onClick={() => navigate("/")} className="navbar-brand">
+          Lifters Shop
+        </span>
         <ul className="navbar-nav">
           <li className="nav-item">Shop</li>
           <li className="nav-item">Stories</li>
@@ -27,7 +26,7 @@ const Navbar: React.FC<NavbarProps> = ({ totalItems }) => {
       <div className="navbar-right">
         <div className="cart-info">
           <Bag size={24} />
-          <span className="cart-count">{totalItems}</span>
+          <span className="cart-count">{state.cart.length}</span>
           <span className="login-text">Login</span>
         </div>
       </div>
